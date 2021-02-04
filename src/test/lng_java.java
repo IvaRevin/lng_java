e15d0f1fc9ce9b58198fdd6794f483d581d37fce
IvaRevin <reviniv@gmail.com>
2021-02-04 17:59:37 +0300

    lng update

--- src/test/lng_java.java
+++ src/test/lng_java.java
@@ -40,48 +40,53 @@ public class lng_java {
             return Objects.hash(first, second, third);
         }
     }
-    public static void main(String[] args) throws IOException{
 
+    public static void main(String[] args) throws IOException {
 
-            Map<Triplet, List<Triplet>> result = Files.readAllLines(Paths.get("/Users/revin/Desktop/тестовое/lng.csv"))
-                    .parallelStream()
-                    .map(l -> l.split(";"))
-                    .map(a -> {
-                        if (a.length == 3) {
-                            return new Triplet(a[0], a[1], a[2]);
-                        }
-                        return null;
-                    })
-                    .filter(t -> t != null)
-                    .distinct()
-                    .collect(Collectors.groupingBy(r -> r, Collectors.mapping(r -> r, Collectors.toList())));
+        long startTime = System.currentTimeMillis();
 
-            AtomicInteger i = new AtomicInteger(0);
+        Map<Triplet, List<Triplet>> result = Files.lines(Paths.get("lng-big.csv"))
+                .map(l -> l.split(";"))
+                .map(a -> {
+                    if (a.length == 3) {
+                        return new Triplet(a[0], a[1], a[2]);
+                    }
+                    return null;
+                })
+                .filter(t -> t != null)
+                .distinct()
+                .collect(Collectors.groupingBy(r -> r, Collectors.mapping(r -> r, Collectors.toList())));
 
-            long count = result.entrySet()
-                    .stream()
-                    .filter(r -> r.getValue().size() > 1)
-                    .count();
+        AtomicInteger i = new AtomicInteger(0);
 
-            Files.write(Paths.get("result.txt"),
-                    new StringBuilder("Records grouping count that greater 1 is ")
-                            .append(count)
-                            .append("\n")
-                            .toString()
-                            .getBytes());
+        long count = result.entrySet()
+                .stream()
+                .filter(r -> r.getValue().size() > 0)
+                .count();
 
-            Files.write(Paths.get("result.txt"),
-                    () -> result
-                            .entrySet()
-                            .stream()
-                            .sorted((o1, o2) -> {
-                                return (o2.getValue().size() > o1.getValue().size()) ? 1 : ((o2.getValue().size() < o1.getValue().size())) ? -1 : 0;
-                            })
-                            .<CharSequence>map(e -> "Group " + i.incrementAndGet() + '\n'
-                                    + e.getValue()
-                                    .stream()
-                                    .map(Object::toString)
-                                    .collect(Collectors.joining("\n"))).iterator(), StandardOpenOption.APPEND);
-        }
+        Files.write(Paths.get("result.txt"),
+                new StringBuilder("Records grouping count that greater 1 is ")
+                        .append(count)
+                        .append("\n")
+                        .toString()
+                        .getBytes());
+
+        Files.write(Paths.get("result.txt"),
+                () -> result
+                        .entrySet()
+                        .stream()
+                        .sorted((o1, o2) -> {
+                            return (o2.getValue().size() > o1.getValue().size()) ? 1 : ((o2.getValue().size() < o1.getValue().size())) ? -1 : 0;
+                        })
+                        .<CharSequence>map(e -> "Group " + i.incrementAndGet() + '\n'
+                                + e.getValue()
+                                .stream()
+                                .map(Object::toString)
+                                .collect(Collectors.joining("\n"))).iterator(), StandardOpenOption.APPEND);
+
+        long timeSpent = System.currentTimeMillis() - startTime;
+
+        System.out.println(timeSpent);
+    }
 }
 
